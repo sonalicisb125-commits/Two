dbf7455bf4ec0e4e02d118cc4d54cc03cef2ce54
SOnali <sonalicisb125@gmail.com>
2025-10-02 19:51:44 +0530

    Thirsty

--- hungry.py
+++ hungry.py
@@ -4,4 +4,6 @@ if hungry == "yes":
     print("Eat pizza")
     print("Eat burger")
 else:
-    print("Continue your work")
+    thirsty = input("Are you thirsty-->")
+    if thirsty == "yes":
+        print("Drink Water")
